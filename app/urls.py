d60387af09acb79ba02c17f0288a645a2715dc7e
VadymPolyanski <vadym.polyanski@gmail.com>
2017-05-12 01:10:07 +0300

    added user service

--- app/urls.py
+++ app/urls.py
@@ -5,12 +5,15 @@ from . import views
 app_name = 'app'
 
 urlpatterns = [
-
     url(r'^$', views.CategoriesView.as_view(), name='main'),
+    url(r'^registration/$', views.UserFormView.as_view(), name='registration'),
+    url(r'^login/$', views.login_user, name='login_user'),
+    url(r'^logout/$', views.logout_user, name='logout_user'),
     url(r'^(?P<slug>[\w-]+)/$', views.DetailCategoryView.as_view(), name='detail_category'),
     url(r'^(?P<category_slug>[\w-]+)/(?P<slug>[\w-]+)$', views.DetailProductView.as_view(), name='detail_product'),
     url(r'^category/add/$', views.CategoryCreate.as_view(), name='category_create'),
     url(r'^last-added/24/$', views.ProductsView.as_view(), name='last_added'),
     url(r'^category/(?P<slug>[\w-]+)/$', views.CategoryUpdate.as_view(), name='category_update'),
     url(r'^category/(?P<slug>[\w-]+)/delete/$', views.CategoryDelete.as_view(), name='category_delete'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,26 +1,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views import generic
+from django.views import generic, View
 from django.views.generic import CreateView, UpdateView, DeleteView
 
+from app.forms import UserForm
 from app.models import Category, Product
-from app.service.date_service import check_datetime
+from app.service.datetime_service import check_datetime
 
-ADDRESS_MAIN_HTML = 'app/main.html'
-ADDRESS_LAST_ADDED_HTML = 'app/last_added.html'
-DETAIL_CATEGORY_ADDRESS = 'app/detail_category.html'
-DETAIL_PRODUCT_ADDRESS = 'app/detail_product.html'
+ADDRESS_MAIN = 'app/main.html'
+ADDRESS_LAST_ADDED = 'app/last_added.html'
 ADDRESS_CREATE_CATEGORY = 'app/create_category.html'
-
+ADDRESS_REGISTRATION = 'app/registration_form.html'
+ADDRESS_DETAIL_CATEGORY = 'app/detail_category.html'
+ADDRESS_DETAIL_PRODUCT = 'app/detail_product.html'
+ADDRESS_LOGIN_USER = 'app/login.html'
 CATEGORY_FIELDS = ['name', 'slug', 'description']
 
 
 class CategoriesView(generic.ListView):
-    template_name = ADDRESS_MAIN_HTML
+    template_name = ADDRESS_MAIN
     context_object_name = 'all_categories'
 
     def get_queryset(self):
@@ -29,12 +33,12 @@ class CategoriesView(generic.ListView):
 
 class DetailCategoryView(generic.DetailView):
     model = Category
-    template_name = DETAIL_CATEGORY_ADDRESS
+    template_name = ADDRESS_DETAIL_CATEGORY
 
 
 class DetailProductView(generic.DetailView):
     model = Product
-    template_name = DETAIL_PRODUCT_ADDRESS
+    template_name = ADDRESS_DETAIL_PRODUCT
 
 
 class CategoryCreate(CreateView):
@@ -56,10 +60,10 @@ class CategoryDelete(DeleteView):
 
 
 class ProductsView(generic.ListView):
-    template_name = ADDRESS_LAST_ADDED_HTML
+    template_name = ADDRESS_LAST_ADDED
     context_object_name = 'all_products'
 
-    @method_decorator(login_required)
+    @method_decorator(login_required(login_url='app:login_user'))
     def dispatch(self, request, *args, **kwargs):
         return super(ProductsView, self).dispatch(request, *args, **kwargs)
 
@@ -71,4 +75,56 @@ class ProductsView(generic.ListView):
                 result.add(product)
         return result
 
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = ADDRESS_REGISTRATION
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('app:main')
+
+        return render(request, self.template_name, {'form': form})
+
+
+def logout_user(request):
+    logout(request)
+    form = UserForm(request.POST or None)
+    context = {
+        "form": form,
+    }
+    return render(request, ADDRESS_LOGIN_USER, context)
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                categories = Category.objects.all()
+                return render(request, ADDRESS_MAIN, {'all_categories': categories})
+            else:
+                return render(request, ADDRESS_LOGIN_USER, {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, ADDRESS_LOGIN_USER, {'error_message': 'Invalid login'})
+    return render(request, ADDRESS_LOGIN_USER)
 
