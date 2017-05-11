0281e45e32a58d15ba03ed6c0405f76e2dc3894b
VadymPolyanski <vadym.polyanski@gmail.com>
2017-05-11 09:30:07 +0300

    created main page with categories and base.html

--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from app.models import Category
+
+ADDRESS_MAIN_HTML = 'app/main.html'
+
+
+class MainView(generic.ListView):
+    template_name = ADDRESS_MAIN_HTML
+    context_object_name = 'all_categories'
+
+    def get_queryset(self):
+        return Category.objects.all()
